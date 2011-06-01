aa7eff3cb167f61e6e59c79a7008e9cf10b64f0e
John Mertens <john.mertens@gmail.com>
2011-06-01 11:23:09 -0700

    Remove config file, add sample config file.

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'philly-phpfogapp-com');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'admin-6471');
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'code4america');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
-define('DB_HOST', 'db01-share');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '0.159605885797221');
-define('SECURE_AUTH_KEY',  '0.159605885797221');
-define('LOGGED_IN_KEY',    '0.159605885797221');
-define('NONCE_KEY',        '0.159605885797221');
-define('AUTH_SALT',        '0.159605885797221');
-define('SECURE_AUTH_SALT', '0.159605885797221');
-define('LOGGED_IN_SALT',   '0.159605885797221');
-define('NONCE_SALT',       '0.159605885797221');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
@@ -88,6 +88,3 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
-/** allows users to upgrade/add plugins without it asking for ftp credentials **/
-define('FS_METHOD', 'direct');
